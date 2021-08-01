cebdf84aa4abeb3b93b9b6897bedbbe1615e3f56
dmitry.chernetsov <dmitry.chernetsov@ntlab.com>
2021-08-01 03:33:46 +0300

    validation for create and update

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,13 +6,13 @@
     <a type="button" class="btn btn-secondary" href="{{route('users.index')}}">Back to users</a>
     <form
         method="POST"
-          @if(isset($user))
-          action="{{route('users.update', $user)}}"
-          @else
-          action="{{route('users.store')}}"
+        @if(isset($user))
+        action="{{route('users.update', $user)}}"
+        @else
+        action="{{route('users.store')}}"
 
-          @endif
-          class="mt-3">
+        @endif
+        class="mt-3">
         @csrf
         @isset($user)
             @method('PUT')
@@ -20,13 +20,19 @@
         <div class="row">
             <div class="col">
                 <input name="name"
-                       value="{{isset($user) ? $user->name : null}}"
+                       value="{{old('name',isset($user) ? $user->name : null) }}"
                        type="text" class="form-control" placeholder="Name" aria-label="name">
+                @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
             <div class="col">
                 <input name="email"
-                       value="{{isset($user) ? $user->email : null}}"
+                       value="{{old('email',isset($user) ? $user->email : null) }}"
                        type="text" class="form-control" placeholder="Email" aria-label="email">
+                @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
             </div>
         </div>
         <div class="row mt-4">
